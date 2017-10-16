b6d36ac3848f0511986ab161d46f4ac8c7b246c7
Erin Brookins Wilcox <brookin8@gmail.com>
2017-10-16 19:06:31 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="app">
+  <h1> Memory </h1>
+    <board v-on:boardClicked="catchBoard" />
+  </div>
+</template>
+
+<script>
+import board from './components/board.vue'
+
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    }
+  },
+  components : {
+    board
+  },
+
+  props : ['cardNumb'],
+
+  methods : {
+    catchBoard : function(cardNumb) {
+      alert(cardNumb)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+  text-align: left;
+  margin-left: 34%;
+  font-size: 400%;
+  margin-bottom: 1%;
+  font-family: 'Gabriela', serif;
+
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/card.vue
@@ -0,0 +1,37 @@
+<template>
+	<div class="card" 
+		v-bind:class="cardNum"
+		v-on:click ="reportClick(cardNum)"></div>
+</template>
+
+<script>
+
+	export default {
+
+		name : 'card', 
+		props : [ 'cardNum'],
+		methods : {
+			reportClick : function(cardNum) {
+			this.$emit('cardClicked', cardNum);
+
+			}
+		}
+	}
+
+</script>
+
+<style>
+	.card {
+		margin : 5px;
+		border : 2px solid #000;
+		width : 125px;
+		height : 125px;
+		background-color : #4BCE7F;
+		display : inline-block;
+		box-shadow:inset 0 0 0 2000px rgba(255, 255, 148,1);
+		background-size: cover;
+	}
+
+	
+
+</style>
